b1e79ad113f2060fbe29db7b460c5181a7883d2c
Diakho Camara <diakho.camara@gmail.com>
2023-05-25 12:40:01 +0200

    Create header and footer

--- wp-content/themes/mota_photo/functions.php
+++ wp-content/themes/mota_photo/functions.php
@@ -1,2 +1,36 @@
 <?php
 
+/**
+ * Enqueue scripts and styles
+ */
+
+function mota_photo_styles_scripts() {
+
+    //styles
+	wp_enqueue_style( 
+        'mota-photo-styles', 
+        get_template_directory_uri() . '/assets/css/style.css',
+        array(),
+        '1.0'
+    );
+
+    //scripts JS
+	wp_enqueue_script( 
+        'mota-photo-scripts',
+        get_template_directory_uri() . '/assets/js/script.js', 
+        array('jquery'),
+        '1.0', 
+        true 
+    );
+}
+add_action( 'wp_enqueue_scripts', 'mota_photo_styles_scripts' );
+
+/**
+ * Register nav menu
+ */
+
+function register_my_menu() {
+    register_nav_menu( 'main-menu', 'Menu principal' );
+}
+add_action( 'after_setup_theme', 'register_my_menu' );
+
---
+++ wp-content/themes/mota_photo/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    
+    <?php wp_body_open(); ?>
+
+    <nav id="navigation">
+        <?php
+        wp_nav_menu(
+        array(
+            'theme_location' => 'main-menu',
+            'menu_id' => 'primary-menu',
+        )
+        );
+        ?>
+    </nav>
