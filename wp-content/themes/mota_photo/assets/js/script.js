6f254557950e8cedec4531cf4927fc01ba875266
Diakho Camara <diakho.camara@gmail.com>
2023-05-26 16:07:47 +0200

    Create header and footer mobile versions

--- wp-content/themes/mota_photo/assets/js/script.js
+++ wp-content/themes/mota_photo/assets/js/script.js
@@ -7,16 +7,39 @@ var linkOpenModal = document.querySelector(".menu-contact-link");
 var contactForm = document.querySelector(".contact-form");
 
 linkOpenModal.addEventListener("click", function() {
-    contactModal.style.display = "block";
+    contactModal.classList.toggle('contact-modal-displayed');
 });
 
 window.addEventListener("click", function(event) {
     if (event.target == contactModal) {
-      contactModal.style.display = "none";
+        contactModal.classList.toggle('contact-modal-displayed');
     }
 });
 
 contactForm.addEventListener("submit", function(event) {
     event.preventDefault();
-    contactModal.style.display = "none";
+    contactModal.classList.toggle('contact-modal-displayed');
 });
+
+/**
+ * MAIN MENU MOBILE
+ */
+
+var mainHeaderMobile = document.querySelector(".main-header-mobile");
+var icone = document.querySelector(".burger-menu-mobile");
+var burgerMenuModal = document.querySelector(".burger-menu-modal");
+var menuItems = document.querySelectorAll('.burger-menu-modal-opened li');
+
+icone.addEventListener("click", function() {
+    burgerMenuModal.classList.toggle('burger-menu-modal-opened');
+    icone.classList.toggle('fa-times');
+});
+
+menuItems.forEach(element => {
+    element.addEventListener('click', function() {
+        modal.classList.toggle('burger-menu-modal-opened');
+        icone.classList.toggle('fa-times');
+    });
+});
+
+console.log(linkOpenModal);
--- wp-content/themes/mota_photo/header.php
+++ wp-content/themes/mota_photo/header.php
@@ -3,26 +3,60 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
-    <div class="main-header">
+
         <header>
-            <div class="main-header-logo">
-                <img src="<?= get_template_directory_uri() . '/assets/images/logo.svg' ?>" alt="Nathalie Mota logo">
+
+            <!--MAIN MENU DESKTOP-->
+            <div class="main-header main-header-desktop">
+                <div class="main-header-logo">
+                    <a href="<?= get_home_url() ?>">
+                        <img src="<?= get_template_directory_uri() . '/assets/images/logo.svg' ?>" alt="Nathalie Mota logo">
+                    </a>
+                </div>
+                <nav class="main-header-nav" id="navigation">
+                    <?php
+                    wp_nav_menu(
+                    array(
+                        'theme_location' => 'main-menu',
+                        'menu_id' => 'primary-menu',
+                    )
+                    );
+                    ?>
+                </nav>
             </div>
-            <nav class="main-header-nav" id="navigation">
-                <?php
-                wp_nav_menu(
-                array(
-                    'theme_location' => 'main-menu',
-                    'menu_id' => 'primary-menu',
-                )
-                );
-                ?>
-            </nav>
+            <!--END MAIN MENU DESKTOP-->
+
+            <!--MAIN MENU MOBILE-->
+            <div class="main-header-mobile">
+                <div class="navbar-head">
+                    <div class="main-header-logo">
+                        <a href="<?= get_home_url() ?>">
+                            <img src="<?= get_template_directory_uri() . '/assets/images/logo.svg' ?>" alt="Nathalie Mota logo">
+                        </a>
+                    </div>
+                    <i class="fas fa-bars burger-menu-mobile"></i>
+                </div>
+                <div class="burger-menu-modal">
+                    <nav>
+                        <?php
+                        wp_nav_menu(
+                            array(
+                                'theme_location' => 'main-menu',
+                                'menu_id' => 'primary-menu',
+                            )
+                        );
+                        ?>
+                    </nav>
+                </div>
+            </div>
+            <!--END MAIN MENU MOBILE-->
+
         </header>
-    </div>
+
     <?php get_template_part('template-parts/contact-modal'); ?>
